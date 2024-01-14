181b50ff3238d546fbec93ce0cb2fda758a5a8a1
filippogiatti <150442166+filippogiatti@users.noreply.github.com>
2024-01-14 22:09:20 +0100

    Add files via upload

---
+++ ModificaDado.java
@@ -0,0 +1,92 @@
+import java.util.Random;
+public class ModificaDado {
+    public static void main(String[] args) {
+        int n, i;
+        int[] frequenza = new int[6];
+
+        for (i = 0; i < 100; i++) {
+            n = ValoreRandom(0, 6);
+            FacceDado(n);
+            frequenza[n - 1]++;
+        }
+
+        for (i = 0; i < frequenza.length; i++) {
+            System.out.printf("Faccia %d: %d\n", i + 1, frequenza[i]);
+        }
+    }
+
+    private static int ValoreRandom(int minValue, int maxValue)
+    {
+        Random casuale = new Random();
+
+        return  casuale.nextInt(minValue,maxValue)+1;
+
+    }
+    private static void FacceDado(int valoreFaccia1)
+    {
+        //ALT + 201 ╔
+        //ALT + 188 ╝
+        //ALT + 187 ╗
+        //ALT + 200 ╚
+        //ALT + 205 ═
+        //ALT + 186 ║
+
+        switch (valoreFaccia1) {
+            case 1 : {
+                System.out.println("\t\t\t\t ╔═════════╗");
+                System.out.println("\t\t\t\t ║         ║");
+                System.out.println("\t\t\t\t ║    O    ║");
+                System.out.println("\t\t\t\t ║         ║");
+                System.out.println("\t\t\t\t ╚═════════╝");
+
+                break;
+            }
+
+            case 2 : {
+                System.out.println("\t\t\t\t ╔═════════╗");
+                System.out.println("\t\t\t\t ║  O      ║");
+                System.out.println("\t\t\t\t ║         ║");
+                System.out.println("\t\t\t\t ║      O  ║");
+                System.out.println("\t\t\t\t ╚═════════╝");
+
+                break;
+            }
+
+            case 3 : {
+                System.out.println("\t\t\t\t ╔═════════╗");
+                System.out.println("\t\t\t\t ║  O      ║");
+                System.out.println("\t\t\t\t ║    O    ║");
+                System.out.println("\t\t\t\t ║      O  ║");
+                System.out.println("\t\t\t\t ╚═════════╝");
+                break;
+            }
+
+            case 4 : {
+                System.out.println("\t\t\t\t ╔═════════╗");
+                System.out.println("\t\t\t\t ║  O   O  ║");
+                System.out.println("\t\t\t\t ║         ║");
+                System.out.println("\t\t\t\t ║  O   O  ║");
+                System.out.println("\t\t\t\t ╚═════════╝");
+                break;
+            }
+
+            case 5 : {
+                System.out.println("\t\t\t\t ╔═════════╗");
+                System.out.println("\t\t\t\t ║  O   O  ║");
+                System.out.println("\t\t\t\t ║    O    ║");
+                System.out.println("\t\t\t\t ║  O   O  ║");
+                System.out.println("\t\t\t\t ╚═════════╝");
+                break;
+            }
+
+            case 6 : {
+                System.out.println("\t\t\t\t ╔═════════╗");
+                System.out.println("\t\t\t\t ║  O   O  ║");
+                System.out.println("\t\t\t\t ║  O   O  ║");
+                System.out.println("\t\t\t\t ║  O   O  ║");
+                System.out.println("\t\t\t\t ╚═════════╝");
+                break;
+            }
+        }
+    }
+}
