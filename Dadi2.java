59eb0faf1771108a92b2de8cf571ef09d4a0ca11
filippogiatti <150442166+filippogiatti@users.noreply.github.com>
2024-01-21 22:27:15 +0100

    Add files via upload

---
+++ Dadi2.java
@@ -0,0 +1,116 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class Dadi2 {
+    public static void main(String[] args)
+
+    {
+        //dichiarazione variabili
+        int dado1=0, dado2=0, caso=0;
+        int scelta = 0;
+        int valoreFaccia=0;
+        boolean exit = true;
+        Scanner scanner = new Scanner(System.in); //richiamo costruttore scanner
+
+        //ciclo per far ripetere la scelta fino a che utente non fa 3
+        do {
+            ClrScr(); //richiamo funzione per cancellare lo schermo
+            Menù(); //richiamo funzione per visualizzare il Menù
+
+            scelta = scanner.nextInt(); // attribuire il valore di scelta
+
+            switch (scelta) { // swich case con la scelta
+                case 1:
+                {
+                    caso=1;
+                    dado1 = estraiDado(); // richiamo metodo estraiDado per attribuire il valore alla variabile
+                    System.out.println("Giocatore 1 ha estratto: " +dado1);
+                    dado2 = estraiDado(); // richiamo metodo estraiDado per attribuire il valore alla variabile
+                    System.out.println("Giocatore 2 ha estratto: " +dado2);
+                    Vincitore(dado1, dado2, caso); // richiamo metodo per dire chi ha vinto
+                    Wait(2000);
+                    break;
+                }
+                case 2:
+                {
+                    caso=2;
+                    dado1 = estraiDado(); // richiamo metodo estraiDado per attribuire il valore alla variabile
+                    System.out.println("Giocatore 1 ha estratto: " +dado1);
+                    dado2 = estraiDado(); // richiamo metodo estraiDado per attribuire il valore alla variabile
+                    System.out.println("Il computer ha estratto: " +dado2);
+                    Vincitore(dado1, dado2, caso); // richiamo metodo per dire chi ha vinto
+                    Wait(2000);
+                    break;
+                }
+                case 3:
+                {
+                    System.out.println("Uscita "); // comunicare l'uscita dal ciclo
+                    exit = false; // attribuire il valore per uscire dal ciclo
+                    Wait(2000);
+                    break;
+                }
+                default: // condizione nel caso nessuna delle 3 precedenti sia vera
+                {
+                    System.out.println("Scelta errata ");
+                    Wait(200);
+                }
+
+            }
+
+        }while (exit); // condizione
+
+
+    }
+
+    private static int estraiDado()  // metodo per estrarre il dado
+    {
+        Random num = new Random();
+        return num.nextInt(6) + 1;
+    }
+
+    private static void Menù () // metodo per visualizzare il menu
+    {
+        System.out.println("=== App lotto === ");
+        System.out.println("[1]-Sfida tra due giocatori");
+        System.out.println("[2] Sfida contro il computer");
+        System.out.println("[3] Exit ");
+    }
+
+    private static void ClrScr(){ // metodo per cancellare lo schermo
+        try {
+            new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void Vincitore(int dado1, int dado2, int caso) //metodo estrarre vincitore
+    {
+
+        if (dado1>dado2)
+            System.out.println("\nHa vinto il G1");
+
+        else if (dado1==dado2)
+            System.out.println("\nPareggio");
+
+        else
+        {
+            if (caso==1)
+                System.out.println("\nHa vinto il G2");
+            else
+                System.out.println("\nHa vinto il Computer");
+        }
+
+    }
+
+
+    private static void Wait(int attesa) // metodo per aspettare x secondi
+    {
+        try
+        {
+            Thread.sleep(attesa);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
