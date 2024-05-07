6718c7371fec9e563654256e9742bda6908b29a1
filippogiatti <150442166+filippogiatti@users.noreply.github.com>
2024-05-07 18:24:31 +0200

    Update Contatto.java

--- src/Libri.java
+++ src/Libri.java
@@ -1,9 +1,13 @@
-enum genere{narrativo,avventura,romanzo,storico}
-public class Libri {
-    public String titolo;
-    public genere tipo;
-    public int anno;
-    public String stampa(){
-        return String.format("titolo: %s  genere: %s  anno: %d",titolo,tipo,anno);
+enum tipoContratto{abitazione, cellulare, aziendale};
+
+public class Contatto {
+    public String nome;
+    public String cognome;
+    public String telefono;
+    public tipoContratto tipo;
+
+    public String stampa()
+    {
+        return String.format("Nome: %s Cognome: %s Telefono: %s, tipo: %s", nome, cognome, telefono, tipo.toString());
     }
 }
